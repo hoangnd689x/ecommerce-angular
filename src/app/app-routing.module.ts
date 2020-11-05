de64092d98285ab3f390f881ba09a8a84ada5801
Lang Thi Xuan Tran (RBVH/ETM44) <lar1hc@bosch.com>
2020-11-05 22:00:30 +0700

    added for payment page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PaymentComponent } from './components/payment/payment.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'checkout', component: PaymentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
